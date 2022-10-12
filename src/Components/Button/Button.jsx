d1665ac7316ccb231e412d276a82298d3a57f8bf
danbaziewicz <danxbazi@gmail.com>
2022-10-12 11:37:59 -0300

    refactor: excluído componente e adiconado diretamente ao form.

--- src/Components/Button/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-
-export default function BasicButtons() {
-  return (
-      <Button variant="contained">Cadastrar</Button>
-  );
-}
