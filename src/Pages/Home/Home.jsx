a58292137549cfcd085decb3dc2865a8f5198692
danbaziewicz <danxbazi@gmail.com>
2022-10-11 21:55:35 -0300

    feat: criação de home base.

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Home = () => {
+    return (
+        <div>
+            <section>
+                <h1>MEDCLOUD</h1>
+            </section>
+        </div>
+    )
+}
