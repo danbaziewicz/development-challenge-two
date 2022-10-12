6325bd40ca7213dc8f8236c43d57f4c36a5e9509
danbaziewicz <danxbazi@gmail.com>
2022-10-12 18:18:13 -0300

    feat: criada nova rota para clientes.

--- front/src/Routes/Routes.jsx
+++ front/src/Routes/Routes.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes as Switch, Route } from "react-router-dom";
 import Header from "../Components/common/Header/Header";
 import Cadastro from "../Pages/Cadastro/Cadastro";
+import Clientes from "../Pages/Clientes/Clientes";
 import { Home } from "../Pages/Home/Home";
 
 const Routes = () => {
@@ -11,6 +12,7 @@ const Routes = () => {
             <Switch>
                 <Route path='/' element={<Home/>}/>
                 <Route path='/cadastro' element={<Cadastro/>}/>
+                <Route path='/clientes' element={<Clientes/>}/>
             </Switch>
         </BrowserRouter>
     )
