cd70e95b9f0ed248a1026e445881180714d269c5
danbaziewicz <danxbazi@gmail.com>
2022-10-12 21:04:39 -0300

    feat: cors adicionado.

--- back/src/app.js
+++ back/src/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import userController from './controller/user-controller.js'
+import cors from 'cors'
 
 const app = express()
 
-app.use(express.json())
 
+app.use(express.json())
+app.use(cors())
 userController(app)
 
 export default app
