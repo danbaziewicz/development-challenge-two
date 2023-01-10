48f105fefc2f024697f11a14dd129ba4e4c42690
danbaziewicz <danxbazi@gmail.com>
2023-01-09 23:15:45 -0300

    feat: adicionado dotenv a conexão.

--- back/src/database/conect.js
+++ back/src/database/conect.js
@@ -1,9 +1,10 @@
 import mysql from "mysql2"
+import 'dotenv/config'
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "123456",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: "challenge_medcloud"
 })
 
