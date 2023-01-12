890f2dd8128584e7f68814b9da27691a8a89f81b
danbaziewicz <danxbazi@gmail.com>
2023-01-11 22:23:02 -0300

    refactor: refatoração de componentes na modal edit.

--- front/src/Components/Modal/ModalEdit.jsx
+++ front/src/Components/Modal/ModalEdit.jsx
@@ -1,88 +1,96 @@
 import { Box, Button, Typography, Modal, TextField } from '@mui/material';
 import DatePicker from "../../Components/DatePicker/DatePicker.jsx"
-import { useContext, useState } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import S from '../Modal/StyleModal.module.css'
 import { UserContext } from '../../context/UserProvider.jsx';
 import { updateUser } from '../../Service/Service.jsx';
 import moment from 'moment';
+import styles from './Styles'
 
 const ModalEdit = (obj) => {
     const dataNascimento = moment(obj.birth_date).utc().format("DD/MM/YYYY")
-    const {dataForm}= useContext(UserContext);
+    const { resetDataForm, getFormattedDate }= useContext(UserContext);
     const [open, setOpen] = useState(false);
-    const [formPut, setFormPut] = useState({
-        name: obj.name,
-        birth_date: "",
-        email: obj.email,
-        address: obj.address
-    })
+    const [formPut, setFormPut] = useState({})
+    useEffect(()=>{
+        setFormPut(obj)
+    }, [open])
 
-    function editUser (e) {
+    async function editUser (e) {
         e.preventDefault()
         const body = {...formPut}
-        body.birth_date = dataForm.birth_date;
-        updateUser(obj.id, body)
+        await updateUser(obj.id, body)
         setOpen(false)
         obj.setReload(true)
+        resetDataForm()
     }
 
-    function handleChangeUpdate(e, key) {
-        setFormPut({...formPut, [key]: e.target.value});
-        console.log(e.target.value)
+    function handleChangeUpdate(value, key) {
+        setFormPut({...formPut, [key]: value});
+    }
+
+    function handleCancel() {
+        setOpen(false)
+        resetDataForm()
     }
 
     return (
         <div>
-            <Button sx={{backgroundColor: '#009adf', borderRadius: '7px'}} variant='contained' onClick={() => setOpen(true)}>Editar</Button>
+            <Button sx={{backgroundColor: '#009adf', borderRadius: '7px'}}
+            variant='contained'
+            onClick={() =>
+            setOpen(true)}>Editar</Button>
             <Modal className={S.divDados} hideBackdrop sx={{ backgroundColor: 'white' }} open={open} onClose={()=>setOpen(false)}>
                 <Box className={S.boxModal}>
                     <div className={S.divClientes}>
                         <h2>Editar</h2>
                     </div>
                     <TextField
-                        sx={{ width: '250px', padding: '10px'}}
+                        sx={styles.textField}
                         disabled
                         id="outlined-basic"
                         label="ID Cliente"
-                        value={obj.id}
+                        value={formPut.id}
                         />
                     <TextField
-                        sx={{ width: '250px', padding: '10px'}}
+                        sx={styles.textField}
                         id="outlined-required"
                         required
                         label="Nome"
                         name="name"
                         variant="outlined"
                         value={formPut.name}
-                        onChange={(e) => handleChangeUpdate(e, "name")} 
+                        onChange={(e) => handleChangeUpdate(e.target.value, "name")}
                         />
+                    <DatePicker
+                        value={formPut.birth_date}
+                        onChange={(value) => {handleChangeUpdate(getFormattedDate(value), "birth_date")}}
+                    />
 
-                    <DatePicker birth_date={obj.birth_date} />
-
-                    <TextField 
-                        sx={{ width: '250px', padding: '10px'}}
+                    <TextField
+                        sx={styles.textField}
                         id="outlined-required"
                         required
                         label="Email"
                         name="email"
                         variant="outlined"
                         value={formPut.email}
-                        onChange={(e) => handleChangeUpdate(e, "email")}
+                        onChange={(e) => handleChangeUpdate(e.target.value, "email")}
                         />
-                    <TextField 
-                        sx={{ width: '250px', padding: '10px'}}
+                    <TextField
+                        sx={styles.textField}
                         required
                         id="outlined-required"
                         label="Endereço"
                         name="address"
                         variant="outlined"
                         value={formPut.address}
-                        onChange={(e) => handleChangeUpdate(e, "address")}
+                        onChange={(e) => handleChangeUpdate(e.target.value, "address")}
                         />
                     <Typography>Confirmar alterações?</Typography>
                     <div className={S.buttons}>
                         <Button sx={{backgroundColor: '#009adf', borderRadius: '7px', margin: '0px 3px'}} variant='contained' onClick={(e) => editUser(e)}>Confirmar</Button>
-                        <Button sx={{borderRadius: '7px', margin: '0px 3px'}} color="error" variant='outlined' onClick={() => setOpen(false)}>Cancelar</Button>
+                        <Button sx={{borderRadius: '7px', margin: '0px 3px'}} color="error" variant='outlined' onClick={handleCancel}>Cancelar</Button>
                     </div>
                 </Box>
             </Modal>
