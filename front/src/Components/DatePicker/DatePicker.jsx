b40cff68c9371efeae3db216a3ac71711976e1a4
danbaziewicz <danxbazi@gmail.com>
2023-01-11 22:22:09 -0300

    refactor: refatoração de datepicker com props.

--- front/src/Components/DatePicker/DatePicker.jsx
+++ front/src/Components/DatePicker/DatePicker.jsx
@@ -1,29 +1,20 @@
-import React, { useContext, useState } from 'react';
+import React from 'react';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
-import { UserContext } from '../../context/UserProvider';
-import moment from 'moment';
 
-export default function MaterialUIPickers(data) {
-  const { dataForm } = useContext(UserContext);
-  const datanasc = moment(data.birth_date).utc().format("YYYY/MM/DD")
-  const [value, setValue] = useState(datanasc);
-
+export default function MaterialUIPickers(props) {
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <Stack sx={{ width: '250px', padding: '10px'}}>
         <DesktopDatePicker
           label="Nascimento"
           inputFormat="DD/MM/YYYY"
-          value={value}
+          value={props.value}
           disableFuture={true}
-          onChange={(newValue) => {
-            setValue(newValue);
-            dataForm.birth_date = `${newValue.$y}/${newValue.$M+1}/${newValue.$D}`
-          }}
+          onChange={props.onChange}
           renderInput={(params) => <TextField {...params} />}
         />
       </Stack>
