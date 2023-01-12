b0dd42f8e7b684f30b14e8ae522bab67af7b6a90
danbaziewicz <danxbazi@gmail.com>
2023-01-12 02:53:46 -0300

    feat: alteração de pasta e adição de novas propriedades de estilo.

--- front/src/Components/Modal/Styles.jsx
+++
@@ -1,8 +0,0 @@
-const styles = {
-    textField: {
-        width: '250px',
-        padding: '10px'
-    }
-}
-
-export default styles;
---
+++ front/src/Styles/Styles.jsx
@@ -0,0 +1,15 @@
+const styles = {
+    textField: {
+        width: '250px',
+        padding: '10px'
+    },
+    button: {
+        backgroundColor: '#009adf',
+        borderRadius: '7px'
+    },
+    buttonError: {
+        borderRadius: '7px'
+    }
+}
+
+export default styles;
